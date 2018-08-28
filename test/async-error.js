a11a373f946a84829ae2e8e2bd46a837bb85bd4e
zqjflash <zhengqingjiang03@163.com>
2018-08-28 20:35:19 +0800

    异步错误堆栈

--- test/async-error.js
+++ test/async-error.js
@@ -1,8 +1,9 @@
-// var VError = require('verror');
+'use strict';
+var VError = require('verror');
 function test() {
-    throw new Error("test error");
-    // var err = new VError("test error");
-    // console.log(err.message);
+    // throw new Error("test error");
+    var err = new VError("test error");
+    console.log(err);
 }
 function main() {
     setImmediate(() => test());
