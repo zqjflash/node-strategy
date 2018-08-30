b83748be9e595872ad4d9005749a80d49cf6f85b
zqjflash <zhengqingjiang03@163.com>
2018-08-30 18:06:45 +0800

    Node.js进程管理

---
+++ node-eyes/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": "off",
+        "no-empty": ["error", {"allowEmptyCatch": true}]
+    }
+};
